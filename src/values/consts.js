2d5f30eaf540c0f2468f8b1146e5ba9e545c1e01
NikFreeman <Classic76@tut.by>
2024-01-30 16:37:03 +0300

    feat: add app const

---
+++ src/values/consts.js
@@ -0,0 +1,10 @@
+export const WELCOME = 'Welcome to the File Manager, Username!';
+export const BYEBYE = 'Thank you for using File Manager, Username, goodbye!';
+export const CURRENTLY ='You are currently in ';
+export const ARG_USERNAME = '--username=';
+export const ERROR ={
+  INPUT: 'Invalid input',
+  OPERATION: 'Operation failed',
+  MANY_ARGS: 'Many arguments "username"',
+  NO_ARG: 'No argument "--username=your_username"'
+}
