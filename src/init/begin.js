0438175539fbfa13e424519dd406b3405fd4186b
NikFreeman <Classic76@tut.by>
2024-01-30 16:38:26 +0300

    feat: validate username arg

---
+++ src/init/begin.js
@@ -0,0 +1,18 @@
+import store from "../values/store.js"
+import { ARG_USERNAME, ERROR } from "../values/consts.js";
+
+export function initApp(){
+  try {  
+  const argsApp = process.argv.slice(2).filter((arg) => arg.startsWith(ARG_USERNAME)); 
+  if (argsApp.length == 0) throw new Error(ERROR.NO_ARG)
+  else if (argsApp.length >= 2) throw new Error(ERROR.MANY_ARGS)
+       else store.username = argsApp[0].slice(ARG_USERNAME.length);
+}
+catch (e) {
+  if (e.name !='Error') {throw e}
+  else {
+    console.error(`${e.name}: ${e.message}`);
+    process.exit(1);
+  }
+}
+}
