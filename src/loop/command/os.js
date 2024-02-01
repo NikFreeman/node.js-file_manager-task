e90e3f3fdcd476160691036b0df54afe32a91f3e
NikFreeman <Classic76@tut.by>
2024-02-01 10:22:43 +0300

    feat: add implementation os command

--- src/loop/command/os.js
+++ src/loop/command/os.js
@@ -1,3 +1,29 @@
-export function os(){
-  console.log('OS');
+import { ERROR, amountOfCPU} from "../../values/consts.js";
+import {EOL, cpus,homedir,userInfo,arch} from 'os'
+
+
+export function os(param){
+  switch (param.trim()) {
+    case "--EOL" :
+      console.log(EOL);
+      break;
+    case "--cpus":
+      const cpu = cpus();
+      console.log(amountOfCPU+cpu.length);
+      cpu.forEach((item) =>{
+        console.log(`model: ${item.model} speed: ${item.speed}`);
+      })
+      break;
+    case "--homedir":
+      console.log(homedir());      
+      break;
+    case "--username":
+      console.log(userInfo().username);
+      break;
+    case "--architecture":
+      console.log(arch());
+      break;
+    default:
+console.log(ERROR.OPERATION);
+  } 
 }
--- src/values/consts.js
+++ src/values/consts.js
@@ -2,6 +2,7 @@ export const WELCOME = 'Welcome to the File Manager, Username!';
 export const BYEBYE = 'Thank you for using File Manager, Username, goodbye!';
 export const CURRENT ='You are currently in ';
 export const ARG_USERNAME = '--username=';
+export const amountOfCPU ='amount of CPUS: ';
 
 export const ERROR ={
   INPUT: 'Invalid input',
