ff1a8a25c07877c08845d8cdcb93641764f91170
NikFreeman <Classic76@tut.by>
2024-02-01 14:52:53 +0300

    refactor: ls and up

--- src/loop/command/ls.js
+++ src/loop/command/ls.js
@@ -1,15 +1,18 @@
-import store from "../../values/store.js";
+import { currentDir } from '../../helpers/currentDir.js';
 import { readdir } from 'node:fs/promises';
-import { TYPE_DIRECTORY,TYPE_FILE } from "../../values/consts.js";
+import { TYPE_DIRECTORY,TYPE_FILE } from '../../values/consts.js';
 
 
 export async function ls(){
   const result =[];
-  const dirContent = await readdir(store.currently_dir.join(store.sep),{withFileTypes:true});
-  const directories = dirContent.filter((item)=>item.isDirectory());
-  console.log(directories)
-  directories.forEach((item)=> result.push({name:item.name, type: TYPE_DIRECTORY}));
-  const files = dirContent.filter((item)=> item.isFile()).sort();
-  files.forEach((item)=> result.push({name:item.name, type: TYPE_FILE}));;
+  const dirContent = await readdir(currentDir(),{withFileTypes:true});
+  const directories = dirContent
+    .filter((item)=>item.isDirectory())
+    .sort()
+    .forEach((item)=> result.push({name:item.name, type: TYPE_DIRECTORY}));  
+  const files = dirContent
+    .filter((item)=> item.isFile())
+    .sort()
+    .forEach((item)=> result.push({name:item.name, type: TYPE_FILE}));;
   console.table(result);  
 }
--- src/loop/command/up.js
+++ src/loop/command/up.js
@@ -1,3 +1,7 @@
-export function up(){
-  console.log('UP');
+import store from '../../values/store.js';
+
+export function up(){  
+  if (process.platform =='win32') {
+    if (store.currently_dir.length>1) store.currently_dir.pop()
+  }
 }
