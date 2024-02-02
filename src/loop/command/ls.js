5d8bfc8b9fdb994769f341da54cd2b9d6ea987f9
NikFreeman <Classic76@tut.by>
2024-02-02 16:26:21 +0300

    refactor: ls command

--- src/loop/command/ls.js
+++ src/loop/command/ls.js
@@ -1,18 +1,23 @@
 import { currentDir } from '../../helpers/currentDir.js';
 import { readdir } from 'node:fs/promises';
-import { TYPE_DIRECTORY,TYPE_FILE } from '../../values/consts.js';
+import { TYPE_DIRECTORY,TYPE_FILE, ERROR } from '../../values/consts.js';
 
 
 export async function ls(){
+  try {
   const result =[];
   const dirContent = await readdir(currentDir(),{withFileTypes:true});
-  const directories = dirContent
+  dirContent
     .filter((item)=>item.isDirectory())
     .sort()
     .forEach((item)=> result.push({name:item.name, type: TYPE_DIRECTORY}));  
-  const files = dirContent
+  dirContent
     .filter((item)=> item.isFile())
     .sort()
     .forEach((item)=> result.push({name:item.name, type: TYPE_FILE}));;
   console.table(result);  
+  }
+  catch {
+console.log(ERROR.OPERATION);
+  }
 }
