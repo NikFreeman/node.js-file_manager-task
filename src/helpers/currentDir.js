5c0aef81a80c83de08560dd01193a247d3ec0b86
NikFreeman <Classic76@tut.by>
2024-02-02 16:24:53 +0300

    fix: currentDir for different OS

--- src/helpers/currentDir.js
+++ src/helpers/currentDir.js
@@ -1,8 +1,14 @@
 import store from "../values/store.js";
 
 export function currentDir() {
-  if (process.platform == 'win32' && store.currently_dir.length>1)
-    return store.currently_dir.join(store.sep);
-  else
-    return `${store.currently_dir[0]}${store.sep}`
+  if (process.platform == 'win32') 
+    if (store.currently_dir.length>1) 
+      return store.currently_dir.join(store.sep);
+    else 
+     return `${store.currently_dir[0]}${store.sep}`;
+  else 
+    if (store.currently_dir.length>0) 
+      return store.currently_dir.join(store.sep)
+    else 
+      return `${store.sep}`    
 }
