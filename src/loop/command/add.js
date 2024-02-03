39adb4fbf8b522fb78012765c26b802c82410959
NikFreeman <classic76@tut.by>
2024-02-03 20:28:22 +0300

    feat: add implementation add command

--- src/loop/command/add.js
+++ src/loop/command/add.js
@@ -1,3 +1,22 @@
-export function add(){
-  console.log('ADD');
+import {open} from  'fs/promises';
+import { ERROR } from '../../values/consts.js';
+import {join} from 'path';
+import { currentDir } from '../../helpers/currentDir.js';
+
+
+export async function add(params){
+  const regEx = /[\<\>\:\?\"\/\\\|\*]/g
+ 
+  if(!regEx.test(params)) {
+    try {
+    const fileToCreate = await open(join(currentDir(),params),"w");
+    fileToCreate.close();
+    }
+    catch {
+      console.log(ERROR.OPERATION);
+    }
+  }
+  else {
+    console.log(ERROR.OPERATION)
+}
 }
