b7d7963012db84eaef8c17a1b8612c4ae0736c58
NikFreeman <classic76@tut.by>
2024-02-03 20:35:04 +0300

    refactor: add helper checkFilename

--- src/loop/command/add.js
+++ src/loop/command/add.js
@@ -2,12 +2,13 @@ import {open} from  'fs/promises';
 import { ERROR } from '../../values/consts.js';
 import {join} from 'path';
 import { currentDir } from '../../helpers/currentDir.js';
+import {checkFilename} from '../../helpers/checkFilename.js'
 
 
 export async function add(params){
-  const regEx = /[\<\>\:\?\"\/\\\|\*]/g
+  
  
-  if(!regEx.test(params)) {
+  if(checkFilename(params)) {
     try {
     const fileToCreate = await open(join(currentDir(),params),"w");
     fileToCreate.close();
