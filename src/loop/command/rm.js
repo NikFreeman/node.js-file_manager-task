4d94fcf41a6f221d51654031ca31691f2a38d68f
NikFreeman <Classic76@tut.by>
2024-02-04 13:40:07 +0300

    feat: add implementation rm command

--- src/loop/command/rm.js
+++ src/loop/command/rm.js
@@ -1,3 +1,23 @@
-export function rm(){
-  console.log('RM');
+import {rm as remove} from 'fs/promises';
+import { parseParamsString } from '../../helpers/parseParamsString.js';
+import { buildPath } from '../../helpers/buildPath.js';
+import { CustomError } from '../../values/errors.js';
+import { ERROR } from '../../values/consts.js';
+
+
+export async function rm(params){
+  const {firstParam } = parseParamsString(params);
+  let fileToRemove;
+  if (firstParam !='')
+  fileToRemove = firstParam;
+  else throw new CustomError(ERROR.INPUT);
+  const pathToFile = buildPath(fileToRemove);
+  try {
+    await remove(pathToFile);
+  }
+  catch {
+    throw new CustomError(ERROR.OPERATION);
+  }
+
+
 }
