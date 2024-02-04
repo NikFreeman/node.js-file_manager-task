07a6567647b1d8cf67d0ee069f68d0255c0a4895
NikFreeman <Classic76@tut.by>
2024-02-04 14:03:53 +0300

    feat: add implementation hash command

--- src/loop/command/hash.js
+++ src/loop/command/hash.js
@@ -1,3 +1,32 @@
-export function hash(){
-  console.log('HASH');
+import {open} from 'fs/promises';
+import { createHash } from 'crypto';
+import { pipeline } from 'stream/promises';
+import { stdout } from 'process';
+import { EOL } from 'os';
+import { parseParamsString } from '../../helpers/parseParamsString.js';
+import { buildPath } from '../../helpers/buildPath.js';
+import { CustomError } from '../../values/errors.js';
+import { ERROR } from '../../values/consts.js';
+
+
+export async function hash(params){
+  const {firstParam } = parseParamsString(params);
+  let fileToHash;
+
+  if (firstParam !='')
+  fileToHash = firstParam;
+  else throw new CustomError(ERROR.INPUT);
+
+  const pathToFile = buildPath(fileToHash);
+  
+  try {
+    const fd = open(pathToFile,'r');
+    const readibleStream = (await fd).createReadStream();
+    const hash = createHash('sha256');
+    await pipeline(readibleStream,hash.setEncoding('hex'),stdout,{end:false});
+    stdout.write(EOL);
+  }
+  catch {
+    throw new CustomError(ERROR.OPERATION);
+  }  
 }
